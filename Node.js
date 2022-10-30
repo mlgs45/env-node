ef98792e8923a39a693d5ca68f622f988cf0b9ca
mlgs <mathieu.langlois@voxm.live>
2022-10-30 12:39:32 +0100

    first commit

---
+++ Node.js
@@ -0,0 +1,5 @@
+require('dotenv').config() // this line is mandatory when you want to read variables from the '.env' file
+
+function Node() {
+return console.log(`I am ${process.env.MY_NAME}, wilder in ${process.env.MY_CITY}, and I love ${process.env.MY_LANGUAGE}`);
+}
